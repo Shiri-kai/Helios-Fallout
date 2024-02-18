4bc4a207fcd1bd9b19a894c088699eaeb85f8fc1
Shiri-kai <160087456+Shiri-kai@users.noreply.github.com>
2024-02-18 15:15:11 -0500

    Update sh_recbladedboard.lua

--- Redone Recipes/sh_recbladedboard.lua
+++ Redone Recipes/sh_recbladedboard.lua
@@ -6,7 +6,7 @@ RECIPE.category = "[-Upgrades-]"
 RECIPE.requirements = {
 	["craftingsteel"] = 8,
         ["craftingleather"] = 2,
-        ["craftingwood"] = 3,
+        ["woodenboard"] = 3,
 }
 RECIPE.results = {
 	["bladedboard"] = 1
