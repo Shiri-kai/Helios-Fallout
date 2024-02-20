fc8edbbb8d30a1f4a261c910e176c21e7c7d52e8
Shiri-kai <160087456+Shiri-kai@users.noreply.github.com>
2024-02-20 16:04:43 -0500

    New melee weapons to match new crafting recipes

---
+++ New Weapons/sh_scrapmachete.lua
@@ -0,0 +1,15 @@
+ITEM.name = "Scrap Machete"
+ITEM.description = "The machete is a broad, single-edged blade with a wooden grip wrapped in cloth. It has a medium swinging speed and is swung in alternating diagonal slashes. This one seems to be fashioned from scrap metal and wood"
+ITEM.model = "models/halokiller38/fallout/weapons/melee/machete.mdl"
+ITEM.price = 20
+ITEM.class = "weapon_machete"
+ITEM.weaponCategory = "melee"
+ITEM.category = "Melee"
+ITEM.flag = "v"
+ITEM.width = 1
+ITEM.height = 3
+ITEM.iconCam = {
+	pos = Vector(196.84, 46.1, -8.89),
+	ang = Angle(-6.46, 193.39, 0),
+	fov = 2.98
+}
---
+++ New Weapons/sh_warclub.lua
@@ -0,0 +1,15 @@
+ITEM.name = "Warclub"
+ITEM.description = "An intricately carved but dangerous war club"
+ITEM.model = "models/visualitygaming/fallout/weapons/w_warclub.mdl"
+ITEM.class = "weapon_warclub"
+ITEM.price = 50
+ITEM.weaponCategory = "melee"
+ITEM.category = "Melee"
+ITEM.flag = "v"
+ITEM.width = 1
+ITEM.height = 2
+ITEM.iconCam = {
+	pos = Vector(141.15, 140.86, -9.34),
+	ang = Angle(-5.28, 224.98, 0),
+	fov = 1.83
+}
