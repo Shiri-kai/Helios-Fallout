5ffaa46aeef7355c5fbbaab80e2ea3a8fe2a806f
Shiri-kai <160087456+Shiri-kai@users.noreply.github.com>
2024-02-20 16:03:39 -0500

    Proposed Melee Weapon Crafting

---
+++ Melee Recipes/sh_recrebarclub.lua
@@ -0,0 +1,24 @@
+
+RECIPE.name = "Schematic: 'Rebar Club'."
+RECIPE.description = "*The schematic before you has a list of instructions that you need to follow to craft this item*."
+RECIPE.model = "models/mosi/fallout4/props/junk/technicaldocument.mdl"
+RECIPE.category = "[-Weaponsmithing-]"
+RECIPE.requirements = {
+	["craftingwood"] = 2, 
+	["craftingsteel"] = 7,
+	["craftingconcrete"] = 4,
+	["craftingleather"] = 1,
+}
+RECIPE.results = {
+	["rebarclub"] = 1
+}
+
+RECIPE:PostHook("OnCanCraft", function(recipeTable, client)
+	for _, v in pairs(ents.FindByClass("ix_station_weaponsworkbench")) do
+		if (client:GetPos():DistToSqr(v:GetPos()) < 100 * 100) then
+			return true
+		end
+	end
+
+	return false, "You need to be near a weapons workbench."
+end)
